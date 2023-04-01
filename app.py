f37c57b99f3056c9c81f965e1fae37cd2709ce10
Joseph Owino <89513250+josephotienoowino@users.noreply.github.com>
2023-04-01 16:21:44 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+from flask_login import current_user
+
+file = open("pickle/model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
+
+app = Flask(__name__, static_folder='static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'mysecretkey'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+with app.app_context():
+    db.create_all()  
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('signup'))
+        else:
+            new_user = User(username=username, email=email, password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully')
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have been logged out.')
+    return redirect(url_for('login'))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        url = request.form["url"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+if __name__ == "__main__":
+    app.run(debug=True)
